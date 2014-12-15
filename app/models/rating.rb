9c107636d4bbb7b61d78e9ca7f4c17cfd69e7157
Dylan Lewis <dylanlewis@Dylans-MacBook-Air.local>
2014-12-14 21:52:41 -0500

    Small changes to views & validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,5 @@ class Rating < ActiveRecord::Base
 	belongs_to :activity
 
 	validates :score, :activity, :park, presence: true
+	validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
