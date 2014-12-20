c06e8169dc3604def322534fb232ec9a885af91a
Dylan Lewis <dylanlewis@Dylans-MacBook-Air.local>
2014-12-20 14:08:09 -0500

    Fixed thumbnail display of activity images & fixed controller handling of activity updates

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -45,6 +45,6 @@ class RatingsController < ApplicationController
 	end
 
 	def rating_params
-		params.require(:rating).permit(:score, :activity, :park)
+		params.require(:rating).permit(:score, :activity_id, :park_id)
 	end
 end
